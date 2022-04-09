501862c1793d85a5714e1ebb0522c2885ec14676
Nikhil <nikhilmah23@gmail.com>
2022-04-09 17:28:11 +0530

    Updates

---
+++ precache-manifest.b445f559fd0d3eb8817f26012a63e3f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f22ebde7827fd92b368e8841ca621a7",
+    "url": "/eact-covid-tracker-master/index.html"
+  },
+  {
+    "revision": "fae6c879161d9bac6365",
+    "url": "/eact-covid-tracker-master/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "33b5ede17503e0b5a31e",
+    "url": "/eact-covid-tracker-master/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "fae6c879161d9bac6365",
+    "url": "/eact-covid-tracker-master/static/js/2.8c67d241.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/eact-covid-tracker-master/static/js/2.8c67d241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b5ede17503e0b5a31e",
+    "url": "/eact-covid-tracker-master/static/js/main.7e5b69fd.chunk.js"
+  },
+  {
+    "revision": "a9964905c426d0bf747f",
+    "url": "/eact-covid-tracker-master/static/js/runtime-main.e9f41e5a.js"
+  }
+]);
